9252a097c86de3b43b506f0a03f9506d9d8248d1
pankaj <pnegi8958@gmail.com>
2018-01-07 17:04:00 +0530

    FPS sample initial Base
    Initial commit with base spawner of eney and player movement

---
+++ Assets/Script/EnemySpawnmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnmanager : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public Transform playerObj;
+	GameObject enemyObj = null;
+	Transform enemyTransform;
+
+	// Use this for initialization
+	void Start () {
+		if (enemyPrefab) {
+			
+			enemyTransform = enemyPrefab.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (enemyPrefab) {
+			enemyObj = Instantiate (enemyPrefab.gameObject, transform.position ,Quaternion.identity,this.transform);
+		}
+	}
+}
---
+++ Assets/Script/enemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+	public GameObject target;
+	//Transform targetTransform;
+	//public Transform startMarker;
+	Transform endMarker;
+	public float speed = 0.02F;
+	private float startTime;
+	private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+		if (target) {
+			endMarker = target.transform;
+			startTime = Time.time;
+			journeyLength = Vector3.Distance(this.transform.position, endMarker.position);
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target) {
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector3.Lerp(this.transform.position, endMarker.position, fracJourney);
+		}
+	}
+}
