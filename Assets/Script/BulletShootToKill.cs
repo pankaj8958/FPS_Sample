56a3a0541b6645eeaa849089323809daa7e808a9
pankaj <pnegi8958@gmail.com>
2018-01-09 00:43:32 +0530

    Gun asset and bullet script added
    Added Bullet script and gun Assets

---
+++ Assets/Script/BulletShootToKill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShootToKill : MonoBehaviour {
+
+    Transform objTransform;
+    Vector3 currentPos;
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        objTransform = this.transform;
+        currentPos = objTransform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(objTransform)
+        {
+            this.GetComponent<Rigidbody>().velocity = this.transform.forward * 6;
+        }
+	}
+}
---
+++ Assets/Script/ShootManShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootManShoot : MonoBehaviour
+{
+    public GameObject bulletRef;
+    GameObject newBullet;
+
+
+    void OnMouseDown()
+    {
+        if(bulletRef)
+        {
+            newBullet = Instantiate(bulletRef);
+        }  
+    }
+
+}
